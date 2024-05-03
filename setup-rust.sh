83afaa0927fc9896b138491a0c830960fffc6b2d
Jos Dehaes <jos.dehaes@gmail.com>
2024-05-04 00:18:22 +0200

    fix keyring

--- setup-rust.sh
+++ setup-rust.sh
@@ -2,6 +2,4 @@
 
 rustup install stable
 rustup install nightly
-rustup component add rust-analyzer
-ln -sf $($HOME/.cargo/bin/rustup which --toolchain stable rust-analyzer) "$HOME/.cargo/bin/rust-analyzer"
 echo export PATH="$PATH:$HOME/.cargo/bin" >>"$HOME/.bashrc"
--- setup-vim.sh
+++ setup-vim.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-RUN git clone -b minimal https://github.com/joske/astronvim_v4 ~/.config/nvim
+git clone -b minimal https://github.com/joske/astronvim_v4 ~/.config/nvim
 
 echo "alias vim=nvim" >>"$HOME/.bashrc"
