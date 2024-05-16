45c663ca006991105a1d058f6ed885c6829cde76
Jos Dehaes <jos.dehaes@gmail.com>
2024-05-16 23:34:18 +0200

    add lg alias

--- setup-fish.sh
+++ setup-fish.sh
@@ -3,3 +3,4 @@ mkdir -p /home/rust/.config/fish/conf.d/
 /usr/bin/fish -c "fisher install IlanCosman/tide@v6"
 /usr/bin/fish -c "tide configure --auto --style=Rainbow --prompt_colors='True color' --show_time='24-hour format' --rainbow_prompt_separators=Angled --powerline_prompt_heads=Sharp --powerline_prompt_tails=Flat --powerline_prompt_style='One line' --prompt_spacing=Compact --icons='Many icons' --transient=No"
 /usr/bin/fish -c "alias -s vim nvim"
+/usr/bin/fish -c "alias -s lg lazygit"
